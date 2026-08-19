542d364bc95898004c43ffcda84e7f081f8e2cb9
Joel R <j.mendel.rathus@gmail.com>
2026-08-19 15:34:21 +1000

    with test

--- au_unemployment.py
+++ au_unemployment.py
@@ -1,7 +1,8 @@
 """Plot Australia's latest unemployment rate from the ABS Data API."""
 import pandas as pd
 import matplotlib.pyplot as plt
-
+#bnvvvjfgfhgfghfh
+# test
 URL = "https://data.api.abs.gov.au/rest/data/ABS,LF,1.0.0/M13.3.1599.20.AUS.M?startPeriod=2015-01&format=csv"
 
 df = pd.read_csv(URL, usecols=["TIME_PERIOD", "OBS_VALUE"]).sort_values("TIME_PERIOD")
